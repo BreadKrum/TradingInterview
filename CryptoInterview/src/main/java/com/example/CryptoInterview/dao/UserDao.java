77838ef5014b57a68ff1595c4364499142310b7c
Krum Georgiev <11georgievkr@gmail.com>
2025-07-09 23:42:04 +0100

    Delete CryptoInterview directory

--- CryptoInterview/src/main/java/com/example/CryptoInterview/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-//package com.example.CryptoInterview.dao;
-//
-//import com.example.CryptoInterview.model.User;
-//
-//public interface UserDao {
-//    User getUserById(int id);
-//    void updateBalance(int userId, double newBalance);
-//    void resetBalance(int userId, double startingBalance);
-//}
